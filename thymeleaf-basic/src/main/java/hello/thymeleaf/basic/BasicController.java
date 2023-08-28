0f04c00a5aa5ce9c588993194d7b4b9c38f51d1f
eugene0318 <nttott@gmail.com>
2023-08-29 06:23:09 +0900

    ~thymeleaf literal

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,15 +1,18 @@
 package hello.thymeleaf.basic;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
 
 @Controller
@@ -46,6 +49,39 @@ public class BasicController {
 		model.addAttribute("userMap", map);
 
 		return "basic/varialbe";
+	}
+
+	@GetMapping("/basic-objects")
+	public String basicObjects(HttpSession session) {
+		session.setAttribute("sessionData", "Hello Session");
+		return "basic/basic-objects";
+	}
+
+	@Component("helloBean")
+	static class HelloBean {
+		public String hello(String data) {
+			return "Hello " + data;
+		}
+	}
+
+	@GetMapping("date")
+	public String date(Model model) {
+		model.addAttribute("localeDateTime", LocalDateTime.now());
+		return "basic/date";
+	}
+
+	@GetMapping("link")
+	public String link(Model model) {
+		model.addAttribute("param1", "data1");
+		model.addAttribute("param2", "data2");
+		return "basic/link";
+
+	}
+
+	@GetMapping("/literal")
+	public String literal(Model model) {
+		model.addAttribute("data", "spring!!");
+		return "basic/literal";
 	}
 
 	@Data
